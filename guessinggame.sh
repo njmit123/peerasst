edeade64b06a7763552b75d079aa7695b76f7845
njmit123 <nileshnj993@gmail.com>
2019-06-03 23:40:52 +0530

    Can now take care of non integer input!

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,24 +4,32 @@ echo "Guess number of files"
 
 function guess {
 	read inp
+	val='^[0-9]+$'
 
 	while [[ $inp != $numfiles ]]
 	do
 		echo "Your guess was $inp"
-		if [[ $inp -lt $numfiles ]]
+		if ! [[ $inp =~ $val ]]
 		then
-			echo "Too low!"
+			echo "Please enter integer input only!"
 			read inp
 		elif [[ $inp -gt $numfiles ]]
 		then
 			echo "Too high!"
 			read inp
+		elif [[ $inp -lt $numfiles ]]
+		then
+			echo "Too low!"
+			read inp
+		
 		fi
 	done
-	
+
 	if [[ $inp -eq $numfiles ]]
 	then
 		echo "Right guess!"
+
+
 	fi
 }
 
